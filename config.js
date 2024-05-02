61b66433b3fc99be6ecc991ec079d0cb40a61f0e
st-j0 <136988413+st-j0@users.noreply.github.com>
2024-05-02 14:28:36 -0400

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "༺█▬█ █ ▀█▀༻▓ 𝐀𝐑𝐈𝐄𝐋 ▒";
+global.owner = ['50942630567'];
+global.premium = ['50942630567'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
 global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
 global.sessionName = 'session';
-global.prefa = [".", "!", "/", "?"];
+global.prefa = [".", "!", "-", "?"];
 global.typemenu = "v1"
 global.menuType = "2"
 global.autoswview = true
